5b57ba89a522ba2d71da00743aa769394f568a13
SkeletonsAreBad <ok@skel.cc>
2020-05-31 17:17:33 +1000

    Update info command description

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,7 @@ const { embedInfo } = require('../modules/embed');
 
 module.exports = {
 	name: 'info',
-	description: 'Get technical information about the paranoid android.',
+	description: 'Get technical information about the bot.',
 	cooldown: 10,
 	execute(message, args) {
 		embedInfo(message.client).then((infoEmbed) =>
